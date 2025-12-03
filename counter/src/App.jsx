0175e3767c41656b11976861043f823a8f7a0a0a
arpan7sarkar <arpansakar@gmail.com>
2025-12-04 00:49:27 +0530

    added counter

---
+++ counter/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Counter from './Counter'
+
+const App = () => {
+  return (
+    <div className='bg-black h-screen w-full text-2xlf flex items-center justify-center'>
+      <Counter />
+
+    </div>
+  )
+}
+
+export default App
