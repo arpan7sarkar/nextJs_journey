c878a098e975598d974053305a1b7a6bd974175f
arpan7sarkar <arpansakar@gmail.com>
2025-12-05 21:56:31 +0530

    started with queue management project

---
+++ queue-management/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css"
+
+const App = () => {
+  return (
+    <header>
+      <h1>Queue Management</h1>
+      <p>Manage your customers efficiently</p>
+    </header>
+  )
+}
+
+export default App
