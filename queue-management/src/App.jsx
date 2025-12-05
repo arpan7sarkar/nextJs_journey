23da09403d2891f4a70d8dd8d4dc3f40913bed3a
arpan7sarkar <arpansakar@gmail.com>
2025-12-05 22:45:41 +0530

    added add queue section

--- queue-management/src/App.jsx
+++ queue-management/src/App.jsx
@@ -1,12 +1,27 @@
-import "./App.css"
+import { useState } from "react";
+import "./App.css";
+import QueueForm from "./components/QueueForm";
 
 const App = () => {
+  const [queue, setQueue] = useState([]);
+  const addToQueue = (customers) => {
+    setQueue([...queue, { ...customers, id: Date.now(), status: "waiting" }]);
+  };
+  const removeFromQueue = (id) => {};
+  const updateStatus = (id, newStatus) => {};
   return (
-    <header>
-      <h1>Queue Management</h1>
-      <p>Manage your customers efficiently</p>
-    </header>
-  )
-}
+    <div>
+      <header>
+        <h1>Queue Management</h1>
+        <p>Manage your customers efficiently</p>
+      </header>
 
-export default App
+      <main>
+        <QueueForm onAdd={addToQueue} />
+        <h1>Queue display</h1>
+      </main>
+    </div>
+  );
+};
+
+export default App;
