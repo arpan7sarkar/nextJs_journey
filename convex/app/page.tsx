28fc8e53bd05bff24f2f3ee03ce61ee15ea3180c
arpan7sarkar <arpansakar@gmail.com>
2026-02-23 19:12:45 +0530

    convex setup done

--- convex/app/page.tsx
+++ convex/app/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
-
+import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
-    <div className="flex flex-col">
-      Hello World
+    <div className="flex flex-col h-screen justify-center items-center">
+      <Button>Herllo world</Button>
     </div>
   );
 }
